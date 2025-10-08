22287547627600c9474a5a6225386685d7245ff7
Krrish-xD <krrishatdev@gmail.com>
2025-10-08 14:07:37 +0530

    Create 412-FizzBuzz

---
+++ 412-FizzBuzz/412-FizzBuzz.py
@@ -0,0 +1,4 @@
+# Last updated: 08/10/2025, 14:07:36
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        return ["FizzBuzz" if i%15 == 0 else "Fizz" if i%3 == 0 else "Buzz" if i%5 == 0 else str(i) for i in range(1, n+1)]
